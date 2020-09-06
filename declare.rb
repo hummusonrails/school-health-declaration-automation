5b0857df0440e126d1b411fbb382f3a33b07e98a
benhayehudi <bengreenberg@gmail.com>
2020-09-06 20:00:45 +0300

    finished automation script

--- declare.rb
+++ declare.rb
@@ -34,17 +34,57 @@ end
 def fill_out_declaration(page)
   kids_list = page.div(class: /name_student_infile/)
   kids_list.links.each do |kid|
+    if check_already_submitted?(page)
+      puts "Form already submited"
+      next
+    end
     kid.click
     complete_individual_form(page)
   end
 end
 
 def complete_individual_form(page)
-  byebug
-  page.label(:class, 'field-label check_label h8').click # first checkbox
-  page.label(:class, 'field-label check_label h8').click # second checkbox
-  canvas = page.canvas(class: /signature-pad/)
-  # move mouse around x, y coordinates of canvas to create a signature
+  # two checkboxes
+  page.label(:xpath => '/html/body/div[1]/section/div/div[3]/form/div/div[2]/b/div[2]/div/label').click
+  page.label(:xpath => '/html/body/div[1]/section/div/div[3]/form/div/div[2]/b/div[3]/p/label').click
+  
+  # canvas element
+  canvas = page.browser.driver.find_element(:xpath => "/html/body/div[1]/section/div/div[3]/form/div/div[2]/b/div[6]/div[1]/div/canvas")
+  page.browser.driver.action.move_to(canvas, 50, 20).click_and_hold.move_to(canvas, 550, 85).release.perform
+  page.browser.driver.action.move_to(canvas, 200, 85).click_and_hold.move_to(canvas, 75, 43).release.perform
+  page.button(id: /btn_send/).click
+  
+  validate_success(page)
+end
+
+def validate_success(page)
+  check_for_errors(page)
+  
+  if page.label(class: /answer_send color-red/).visible?
+    puts "Sent form successfully"
+  end
+end
+
+def check_for_errors(page)
+  if page.label(class: /fill_answer1 color-red/).visible?
+    puts "First checkbox not checked properly"
+  end
+
+  if page.label(class: /fill_answer2 color-red/).visible?
+    puts "Second checkbox not checked properly"
+  end
+
+  if page.label(class: /fill_sign color-red/).visible?
+    puts "Signature not recorded properly"
+  end
+
+  if page.label(class: /answer_send color-red hidden/).visible?
+    puts "Form not sent successfully"
+  end
+end
+
+def check_already_submitted?(page)
+  page.link(class: 'answer_send  pdf_wrap_create_briut padding-right-lg-x cursor-pointer').present?
 end
 
 call(ENV['URL'])
