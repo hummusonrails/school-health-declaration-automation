a52a055089f6b99a5f2f40d2ec5d5f59909b7a50
benhayehudi <bengreenberg@gmail.com>
2020-11-18 09:42:18 +0200

    linting

--- declare.rb
+++ declare.rb
@@ -7,6 +7,11 @@ require 'nokogiri'
 require 'webdrivers/chromedriver'
 require 'watir'
 
+# Declare health declaration for schools.
+#
+# @example
+#
+# Declare.new.call
 class Declare
   attr_reader :url, :username, :password
 
@@ -21,10 +26,10 @@ class Declare
   end
 
   def call
-    get_sign_in_page
+    go_to_sign_in_page
   end
 
-  def get_sign_in_page
+  def go_to_sign_in_page
     browser = Watir::Browser.new :chrome, headless: false
     browser.goto(@url)
 
@@ -56,7 +61,7 @@ class Declare
   end
 
   def fill_out_declaration(page)
-    kids = page.divs(:class => /name_student_infile/)
+    kids = page.divs(class: /name_student_infile/)
     kids.each do |kid|
       if check_already_submitted?(kid)
         puts 'Form already submited'
@@ -67,14 +72,16 @@ class Declare
     end
   end
 
-  def complete_individual_form(page)
+  def complete_individual_form(page) # rubocop:disable Metrics/AbcSize
     # two checkboxes
     # THESE HAVE BEEN REMOVED FROM THE FORM (maybe temporarily?)
     # page.label(xpath: '/html/body/div[1]/section/div/div[3]/form/div/div[2]/b/div[2]/div/label').click
     # page.label(xpath: '/html/body/div[1]/section/div/div[3]/form/div/div[2]/b/div[3]/p/label').click
 
     # canvas element
-    canvas = page.browser.driver.find_element(xpath: '/html/body/div[1]/section/div/div[3]/form/div/div[2]/div[5]/div[1]/div/canvas')
+    canvas = page.browser.driver.find_element(
+      xpath: '/html/body/div[1]/section/div/div[3]/form/div/div[2]/div[5]/div[1]/div/canvas'
+    )
     page.browser.driver.action.move_to(canvas, 50, 20).click_and_hold.move_to(canvas, 50, 85).perform
     page.browser.driver.action.move_to(canvas, 200, 85).click_and_hold.move_to(canvas, 75, 43).perform
     page.browser.driver.action.move_to(canvas, 100, 34).click_and_hold.move_to(canvas, 264, 81).perform
@@ -102,6 +109,6 @@ class Declare
   end
 
   def check_already_submitted?(kid)
-    kid.link(:class => /answer_send/).present?
+    kid.link(class: /answer_send/).present?
   end
 end
